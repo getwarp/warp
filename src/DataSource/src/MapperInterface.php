667dc4f445a08eacb3227ef61196c31bdf63183c
Constantine Karnaukhov <genteelknight@gmail.com>
2020-02-19 00:10:17 +0400

    feat(mapper): add init(), hydrate() and extract() methods to MapperInterface

--- src/DataSource/src/MapperInterface.php
+++ src/DataSource/src/MapperInterface.php
@@ -7,6 +7,29 @@ namespace spaceonfire\DataSource;
 interface MapperInterface
 {
     /**
+     * Init empty entity object an return pre-filtered data (hydration will happen on a later stage).
+     * Must return tuple [object entity, array entityData].
+     * @param array $data
+     * @return array
+     */
+    public function init(array $data): array;
+
+    /**
+     * Hydrate entity with dataset.
+     * @param object $entity
+     * @param array  $data
+     * @return object
+     */
+    public function hydrate($entity, array $data);
+
+    /**
+     * Extract all values from the entity.
+     * @param object $entity
+     * @return array
+     */
+    public function extract($entity): array;
+
+    /**
      * Converts field value from storage to domain
      * @param string $fieldName
      * @param mixed $storageValue
