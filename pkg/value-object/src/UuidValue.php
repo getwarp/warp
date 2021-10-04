8a9cdd45603fd77e3f58b2df7b89071c7ebb0117
Constantine Karnaukhov <genteelknight@gmail.com>
2021-10-04 20:16:51 +0400

    feat(value-object): check that symfony/uid installed for uuid value object

--- pkg/value-object/src/UuidValue.php
+++ pkg/value-object/src/UuidValue.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace spaceonfire\ValueObject;
 
+use spaceonfire\Exception\PackageMissingException;
 use Symfony\Component\Uid\Uuid;
 
 class UuidValue extends AbstractStringValue
@@ -14,15 +15,26 @@ class UuidValue extends AbstractStringValue
      */
     public static function random(): self
     {
+        self::checkDependency();
+
         return static::new(Uuid::v4());
     }
 
     protected static function validate($value): void
     {
+        self::checkDependency();
+
         parent::validate($value);
 
         if (!Uuid::isValid((string)$value)) {
             throw new \InvalidArgumentException(\sprintf('Expected a value to be a valid uuid. Got: %s.', $value));
+        }
+    }
+
+    private static function checkDependency(): void
+    {
+        if (!\class_exists(Uuid::class)) {
+            throw PackageMissingException::new('symfony/uid', '^5.3');
         }
     }
 }
