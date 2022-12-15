8837a3b9f41541b8e5f8841c9750c28d8f897f29
Constantine Karnaukhov <genteelknight@gmail.com>
2022-12-15 16:26:00 +0400

    feat: run tests on php8.2

--- pkg/collection/src/Iterator/ArrayCacheIterator.php
+++ pkg/collection/src/Iterator/ArrayCacheIterator.php
@@ -37,7 +37,6 @@ final class ArrayCacheIterator implements \Iterator, \Countable
 
         if ($iterator instanceof \ArrayIterator) {
             $array = $iterator->getArrayCopy();
-            // @phpstan-ignore-next-line
             $this->keys = \array_keys($array);
             $this->values = \array_values($array);
             $this->iterator = null;
